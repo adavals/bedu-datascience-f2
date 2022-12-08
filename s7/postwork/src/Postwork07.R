ff56dd1109da1991b4d7c79b491b05ba4a8184b6
adavals <valenzuela.g@gmail.com>
2022-12-07 18:50:21 -0600

    Postwork 7 fix

--- s7/postwork/src/Postwork07.R
+++ s7/postwork/src/Postwork07.R
@@ -17,7 +17,7 @@ url =
   
 # 2. Realiza una gráfica de la serie de tiempo anterior de 2005
   
-  global.ts.subset <- window(global.ts, end = 2004)
+  global.ts.subset <- window(global.ts, end = c(2004,12))
   
   plot(global.ts.subset, 
        main = "Datos de temperatura global", 
@@ -27,7 +27,7 @@ url =
 # 3. Ahora realiza una gráfica de la serie de tiempo anterior, transformando a la
 # primera diferencia:
   plot(diff(global.ts.subset), 
-       main = "Datos de temperatura global", 
+       main = "Datos de temperatura global - transformada a primera diferencia ", 
        xlab = "Tiempo",
        sub = "Enero de 1856 - Diciembre de 2004")  
 
@@ -41,3 +41,21 @@ acf(diff(global.ts.subset), main = "")
 title(main = "Autocorrelación en primera diferencia")
 pacf(diff(global.ts.subset), main = "")
 title(main = "Autocorrelación parcial en primera diferencia")
+
+# 6. De acuerdo con lo observado en las gráficas anteriores, se sugiere un modelo 
+# ARIMA con AR(1), I(1) y MA desde 1 a 4 rezagos.
+# Estima los diferentes modelos ARIMA propuestos:
+
+arima(global.ts.subset, order = c(1, 1, 1))
+arima(global.ts.subset, order = c(1, 1, 2))
+arima(global.ts.subset, order = c(1, 1, 3))
+arima(global.ts.subset, order = c(1, 1, 4))
+
+
+# 7. Con base en el criterio de Akaike, estima el mejor modelo ARIMA y realiza una 
+# predicción de 12 periodos (meses)
+
+fit <- arima(global.ts.subset, order = c(1, 1, 4))
+pr <- predict(fit, 12)$pred 
+
+pr
